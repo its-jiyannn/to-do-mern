01299a781e16f403fe1a91b3bb5d55d4f55c28c0
its-jiyannn <92353683+its-jiyannn@users.noreply.github.com>
2023-05-15 20:54:26 +0530

    Add files via upload

---
+++ todo-mern-main/frontend_jiyan/src/components/About.jsx
@@ -0,0 +1,11 @@
+const About=()=>{
+    return(
+        <>  
+            <h1 style={{color:"orangedRed",fontSize:"3em",fontFamily:"serif"}}>Jiyan Prajapati</h1>
+            <h1 style={{color:"orangedRed",fontSize:"3em",fontFamily:""}}>202212115</h1>
+            <h1>Contact us : xyz@gmail.com</h1>
+        </>
+    )
+}
+
+export default About;
